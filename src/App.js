98b79e135d25ab62b158b888b60eef94a30cfceb
humanityBG <desislav.lesov@gmail.com>
2023-08-08 15:05:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>Simplest Working Calculator</h1>
+        <h1>Simplest Working Calculator by Desislav Lesov</h1>
       </div>
       <form>
         <p ref={resultRef}>
